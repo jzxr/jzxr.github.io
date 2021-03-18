57e0d75b5c5397e267cdab2eb7f48f9a3a96e6ca
audreyquisit <32848069+audreyquisit@users.noreply.github.com>
2021-03-18 16:08:09 +0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "Downloading the relevant python libraries"
+
+pip install praw
+pip install pandas
+pip install seaborn
+pip install matplotlib
+pip install numpy
+pip install textblob
+pip install requests
+pip install bs4
+pip install schedule
+pip install pytz
+pip install tweepy
+pip install datetime 
+pip install schedule
+
+echo "Download Complete"
